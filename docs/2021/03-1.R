d84ddc93abf451194260c266de67697afddb1cb5
EmilHvitfeldt <emilhhvitfeldt@gmail.com>
2021-12-03 09:06:06 -0800

    unify part 1 and 2

--- docs/2021/03-1.R
+++ docs/2021/03-1.R
@@ -3,18 +3,20 @@ input <- readLines("2021/03-input") |>
   strsplit("") |>
   purrr::reduce(rbind)
 
-common_finder <- function(x, fun) {
-  names(which(fun(table(x)) == table(x)))
+common_finder <- function(x, fun, even) {
+  tab <- table(x)
+  if (length(tab) == 2 & tab[1] == tab[2]) return(even)
+  names(which(fun(tab) == tab))
 }
 
-rate_calculator <- function(mat, fun) {
-  apply(mat, MARGIN = 2, FUN = common_finder, fun) |>
+rate_calculator <- function(mat, fun, even) {
+  apply(mat, MARGIN = 2, FUN = common_finder, fun, even) |>
     paste0(collapse = "") |>
     strtoi(base = 2)
 }
 
-gamma <- rate_calculator(input, max)
+gamma <- rate_calculator(input, max, "1")
 
-epsilon <- rate_calculator(input, min)
+epsilon <- rate_calculator(input, min , "0")
 
 gamma * epsilon
