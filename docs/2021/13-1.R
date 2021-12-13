61d79ac3be7f1331a9f0aa3ceb2bb9783caa0a57
EmilHvitfeldt <emilhhvitfeldt@gmail.com>
2021-12-12 21:42:12 -0800

    2021 day 13

---
+++ docs/2021/13-1.R
@@ -0,0 +1,51 @@
+library(tidyverse)
+
+input <- readLines("2021/13-input")
+
+mid <- which(input == "")
+
+points <- input[seq_len(mid - 1)]
+folds <- input[seq(mid + 1, length(input))]
+
+p1 <- function(x) x + 1L
+
+pointss <- str_split(points, ",") %>%
+  map(as.integer) %>%
+  map(p1)
+
+mat <- matrix(
+  FALSE,
+  nrow = pointss %>% map_int(~.x[2]) %>% max(),
+  ncol = pointss %>% map_int(~.x[1]) %>% max()
+)
+
+for (p in pointss) {
+  mat[p[[2]], p[[1]]] <- TRUE
+}
+
+fold <- folds[1]
+
+axis <- str_extract(fold, "[xy]")
+amount <- str_extract(fold, "[0-9]+") %>% as.integer() %>% p1()
+
+if (axis == "y") {
+  folded <- seq(nrow(mat), amount + 1)
+  landed <- seq(amount - length(folded), amount - 1)
+  if (min(landed) == 1) {
+    mat <- mat[landed, ] | mat[folded, ]
+  } else {
+    leftover_mat <- mat[seq_len(min(landed) - 1), ]
+    mat <- rbind(leftover_mat, mat[landed, ] | mat[folded, ])
+  }
+} else {
+  folded <- seq(ncol(mat), amount + 1)
+  landed <- seq(amount - length(folded), amount - 1)
+  if (min(landed) == 1) {
+    mat <- mat[, landed] | mat[, folded]
+  } else {
+    leftover_mat <- mat[, seq_len(min(landed) - 1)]
+    mat <- rbind(leftover_mat, mat[, landed] | mat[, folded])
+  }
+}
+
+sum(mat)
