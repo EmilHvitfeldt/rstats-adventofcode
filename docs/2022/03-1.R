e6c646068705204f62c09dcaa46906d554a06742
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2022-12-02 21:25:22 -0800

    use head and tail

--- docs/2022/03-1.R
+++ docs/2022/03-1.R
@@ -6,22 +6,12 @@ input <- readLines("2022/03-input") |>
 score <- c(letters, LETTERS)
 
 common_split <- function(x) {
-  len <- length(x) / 2
-  intersect(x[seq_len(len)], x[seq_len(len) + len])
+  half <- length(x) / 2
+  intersect(
+    head(x, half), tail(x, half)
+  )
 }
 
 map_chr(input, common_split) |>
-  match(score) |>
-  sum()
-
-input_group <- split(input, rep(seq_len(length(input) / 3), each = 3))
-
-common_group <- function(x) {
-  x[[1]] |>
-   intersect(x[[2]]) |>
-   intersect(x[[3]])
-}
-
-map_chr(input_group, common_group) |>
   match(score) |>
   sum()
