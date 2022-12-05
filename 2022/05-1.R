6fbdf908f20af2b639968a197a6ad2b465ff1dd2
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2022-12-04 21:35:25 -0800

    2022 day 5

---
+++ 2022/05-1.R
@@ -0,0 +1,25 @@
+library(tidyverse)
+
+input <- read_lines("2022/05-input")
+delim <- which(input == "")
+
+cargo <- input[seq(1, delim - 2)]
+insts <- input[seq(delim + 1, length(input))] |>
+  str_extract_all("[0-9]+") |>
+  map(as.integer)
+
+box_loc <- seq(1, (max(nchar(cargo)) + 1) / 4) * 4 - 2
+
+stacks <- map(box_loc, ~ str_sub(cargo, .x, .x)) |>
+  map(setdiff, c("", " ")) |>
+  map(rev)
+
+for (inst in insts) {
+  from <- inst[2]
+  to <- inst[3]
+  amount <- inst[1]
+  stacks[[to]] <- c(stacks[[to]], rev(tail(stacks[[from]], amount)))
+  stacks[[from]] <- head(stacks[[from]], -amount)
+}
+
+purrr::map_chr(stacks, tail, 1) |> paste(collapse = "")
