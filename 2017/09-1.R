8cae9621813cfca6029aa792b2dfbc8959da3478
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2025-12-16 23:12:39 -0800

    better 2017 day 9

--- 2017/09-1.R
+++ 2017/09-1.R
@@ -1,51 +1,13 @@
 library(stringr)
 input <- readLines("2017/09-input")
 
-remove_garbage <- function(input) {
-  garbage <- logical(length(input))
-
-  i <- 1
-  start <- NA
-  end <- NA
-
-  while (i <= length(input)) {
-    cur <- input[i]
-    if (cur == "!") {
-      i <- i + 2
-      next
-    }
-    if (cur == "<" && is.na(start)) {
-      start <- i
-    }
-    if (cur == ">" && !is.na(start)) {
-      end <- i
-
-      garbage[seq(start, end)] <- TRUE
-      start <- NA
-      end <- NA
-    }
-    i <- i + 1
-  }
-
-  input[!garbage]
-}
-
 input <- input |>
-  str_split_1("") |>
-  remove_garbage()
+  str_remove_all("!.") |>
+  str_remove_all("<.*?>")
 
-input <- input[input != ","]
+input <- str_split_1(input, "")
 
-res <- 0
-level <- 0
-
-for (i in input) {
-  if (i == "{") {
-    level <- level + 1
-  } else if (i == "}") {
-    res <- res + level
-    level <- level - 1
-  }
-}
-
-res
+changes <- c("}" = -1, "{" = 1, "," = 0)[input]
+levels <- cumsum(changes)
+starts <- levels[input == "{"]
+sum(starts)
--- 2017/09-2.R
+++ 2017/09-2.R
@@ -1,28 +1,9 @@
 library(stringr)
 input <- readLines("2017/09-input")
-input <- str_split_1(input, "")
 
-garbage <- logical(length(input))
-
-inside <- FALSE
-i <- 1
-
-while (i <= length(input)) {
-  garbage[i] <- inside
-
-  cur <- input[i]
-
-  if (cur == "<") {
-    inside <- TRUE
-  } else if (cur == ">") {
-    inside <- FALSE
-    garbage[i] <- FALSE
-  } else if (cur == "!") {
-    garbage[i] <- FALSE
-    garbage[i + 1] <- FALSE
-    i <- i + 1
-  }
-  i <- i + 1
-}
-
-sum(garbage)
+input |>
+  str_remove_all("!.") |>
+  str_extract_all("<.*?>") |>
+  lapply(\(x) nchar(x) - 2) |>
+  unlist() |>
+  sum()
